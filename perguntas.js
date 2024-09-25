706d9c1581f7acbeb6035060f91f4c9c1389926d
Pamelav16 <128489755+Pamelav16@users.noreply.github.com>
2024-09-25 19:46:02 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,59 +1,59 @@
 criaCartao(
-    'Título 1',
-    'Pergunta 1',
-    'Resposta 1'
+    'conhecimentos gerais',
+    '1. Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    ' Tem entre 4 a 6 litros. São retirados 450 mililitros'
 )
 
 criaCartao(
-    'Título 2',
-    'Pergunta 2',
-    'Resposta 2'
+    'conhecimentos gerais',
+    '2. De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
 )
 
 criaCartao(
-    'Título 3',
-    'Pergunta 3',
-    'Resposta 3'
+    'conhecimentos gerais',
+    '3. De onde é a invenção do chuveiro elétrico?',
+    ' Brasil'
 )
 
 criaCartao(
-    'Título 4',
-    'Pergunta 4',
-    'Resposta 4'
+    'conhecimentos gerais',
+    '4. Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia'
 )
 
 criaCartao(
-    'Título 5',
-    'Pergunta 5',
-    'Resposta 5'
+    'conhecimentos gerais',
+    '5. Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
 )
 
 criaCartao(
-    'Título 6',
-    'Pergunta 6',
-    'Resposta 6'
+    'conhecimentos gerais',
+    '6. Qual o grupo em que todas as palavras foram escritas corretamente?',
+    ' Asterisco, beneficente, meteorologia, entretido'
 )
 
 criaCartao(
-    'Título 7',
-    'Pergunta 7',
-    'Resposta 7'
+    'conhecimentos gerais',
+    '7. Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    ' Dom Quixote'
 )
 
 criaCartao(
-    'Título 8',
-    'Pergunta 8',
-    'Resposta 8'
+    'conhecimentos gerais',
+    '8. Quantas casas decimais tem o número pi?',
+    ' Infinitas'
 )
 
 criaCartao(
-    'Título 9',
-    'Pergunta 9',
-    'Resposta 9'
+    'conhecimentos gerais',
+    '9. Atualmente, quantos elementos químicos a tabela periódica possui?',
+    ' 118'
 )
 
 criaCartao(
-    'Título 10',
-    'Pergunta 10',
-    'Resposta 10'
+    'conhecimentos gerais',
+    '10. Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    'Japão e Serra Leoa'
 )
